730ecd28ad4ed938f472f2f732e3368517ffe871
Dan Digby <dandigby@me.com>
2025-05-03 12:28:15 +0100

    added cpu temp monitor to atlas app

--- atlas/app.py
+++ atlas/app.py
@@ -8,7 +8,9 @@ def index():
     # Get system stats
     cpu = psutil.cpu_percent(interval=1)
     memory = psutil.virtual_memory().percent
-    return render_template('index.html', cpu=cpu, memory=memory)
+    cpu_temp = psutil.sensors_temperatures()
+    temp = cpu_temp['cpu_thermal'][0][1]
+    return render_template('index.html', cpu=cpu, memory=memory, temp=temp)
 
 if __name__ == '__main__':
     app.run(debug=True)
